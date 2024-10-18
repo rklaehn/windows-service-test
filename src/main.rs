15678f7a1febcb409a0f1ec493542f40ca2de0d5
Ruediger Klaehn <rklaehn@protonmail.com>
2024-10-18 16:55:45 +0300

    query config

--- src/main.rs
+++ src/main.rs
@@ -159,18 +159,15 @@ mod munin_service {
     }
 
     fn query_config() -> windows_service::Result<()> {
-        use std::env;
         use windows_service::{
             service::ServiceAccess,
             service_manager::{ServiceManager, ServiceManagerAccess},
         };
     
-        let service_name = env::args().nth(1).unwrap_or("netlogon".to_owned());
-    
         let manager_access = ServiceManagerAccess::CONNECT;
         let service_manager = ServiceManager::local_computer(None::<&str>, manager_access)?;
     
-        let service = service_manager.open_service(service_name, ServiceAccess::QUERY_CONFIG)?;
+        let service = service_manager.open_service(SERVICE_NAME, ServiceAccess::QUERY_CONFIG)?;
     
         let config = service.query_config()?;
         println!("{:#?}", config);
